50363d266fc5e42dc1515153900727988c04aa67
Chris <chriskim80@gmail.com>
2017-06-07 19:25:40 -0400

    Created about module stub

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MapFormPageComponent } from './map-form-page/map-form-page.component';
+import { HeaderComponent } from './header/header.component';
+import { MapPageComponent } from './map-page/map-page.component';
+import { FeaturesPageComponent } from './features-page/features-page.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    MapFormPageComponent,
+    HeaderComponent,
+    MapPageComponent,
+    FeaturesPageComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
