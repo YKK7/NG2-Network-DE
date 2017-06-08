1c260c6c7e97b23e7285cc6255c167afb9e5930e
Chris <chriskim80@gmail.com>
2017-06-07 21:17:36 -0400

    Made some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { MapFormPageComponent } from './map-form-page/map-form-page.component';
@@ -8,6 +11,10 @@ import { HeaderComponent } from './header/header.component';
 import { MapPageComponent } from './map-page/map-page.component';
 import { FeaturesPageComponent } from './features-page/features-page.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { VolunteerComponent } from './volunteer/volunteer.component';
+import { DonateComponent } from './donate/donate.component';
+
 
 @NgModule({
   declarations: [
@@ -17,10 +24,17 @@ import { AboutComponent } from './about/about.component';
     HeaderComponent,
     MapPageComponent,
     FeaturesPageComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    VolunteerComponent,
+    DonateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rootRouterConfig, { useHash: true }),
+    // AgmCoreModule.forRoot({
+    //   apiKey: 'AIzaSyCVX2yvf9wYcaQVJpG1YC8P__g0YVKnhNw'
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
