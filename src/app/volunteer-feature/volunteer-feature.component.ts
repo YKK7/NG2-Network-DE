2d6a113c69e2dbab9f1fec78cdf715d77ca2874e
Molly Arant <mccormick.mollyt@gmail.com>
2017-06-08 17:10:01 -0400

    Fixed merge conflict

---
+++ src/app/volunteer-feature/volunteer-feature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-feature',
+  templateUrl: './volunteer-feature.component.html',
+  styleUrls: ['./volunteer-feature.component.css']
+})
+export class VolunteerFeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
