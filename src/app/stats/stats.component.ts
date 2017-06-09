81c2e2b68a4e2cc5a0fc1f339f54344a4eb2b018
waxdoll23 <jmcgnty23@gmail.com>
2017-06-09 10:07:58 -0400

    Fixed the nav bar load issue and am working on the typescript now

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -12,4 +12,27 @@ export class StatsComponent implements OnInit {
   ngOnInit() {
   }
 
+
+//   $(document).ready(function(){
+//   $.ajax(
+//     {
+//       url: 'https://data.nationalservice.gov/resource/5vrv-88b8.json',
+//       type: "get",
+//       data: {location_name:'Delaware'},
+//       contentType: "application/json; charset=utf-8",
+//       dataType: "json",
+//       cache: false,
+//       success: function (data) {
+//         var trHTML = '';
+//         $.each(data, function (i, item) {
+//           trHTML += '<tr><td>'+ item.location_name   +'</td><td>'+ item.location_type + '</td><td>' + item.value_category + '</td><td>' +  item.value_name +'</td><td>'+ item.location.coordinates[0]+','+ item.location.coordinates[1]  +'</td></tr>';
+//         });
+//         $('#location').append(trHTML);
+//       },
+//       error: function (msg) {
+//         alert(msg.responseText);
+//       }
+//     });
+// })
+
 }
