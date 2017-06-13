463fbaf7ed4a6149cff1da5168805a48ed956d28
Vilena Livinsky <livinsky27@gmail.com>
2017-06-13 17:23:12 -0400

    updating font on stats

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -38,15 +38,15 @@ export class StatsComponent implements OnInit {
           {text: "Coach, referee, supervise sports team", count: "0.06"},
           {text: "Tutor or teach", count: "0.14"},
           {text: "Mentor youth", count: "0.16"},
-          {text: "Be an usher, greeter, or minister", count: "0.13"},
-          {text: "Collect, prepare, distribute or serve food", count: "0.23"},
-          {text: "Collect, make, or distribute clothing", count: "0.15"},
-          {text: "Fundraise or sell items to raise money", count: "0.29"},
-          {text: "Provide counseling, medical care, fire/EMS", count: "0.07"},
-          {text: "Provide general office services", count: "0.15"},
-          {text: "Provide professional or management", count: "0.17"},
-          {text: "Engage in music, performance, or othe", count: "0.08"},
-          {text: "Engage in general labor", count: "0.18"},
+          {text: "Usher, greeter, or minister", count: "0.13"},
+          {text: "Food services", count: "0.23"},
+          {text: "Clothing provision", count: "0.15"},
+          {text: 'Fundraise/sell to raise money', count: "0.29"},
+          {text: "Counseling, med care, fire/EMS", count: "0.07"},
+          {text: "General office services", count: "0.15"},
+          {text: "Professional or management", count: "0.17"},
+          {text: "Music/performance/other", count: "0.08"},
+          {text: "General labor", count: "0.18"},
         ],
         eval: function (item) {return item.count;},
         classed: function (item) {return item.text.split(" ").join("");}
@@ -96,7 +96,7 @@ export class StatsComponent implements OnInit {
                   "font-size": "14px",
                   "font-family": "Source Sans Pro, sans-serif",
                   "text-anchor": "middle",
-                  fill: "white"
+                  fill: "black"
                 },
                 attr: {
                   dy: "20px",
